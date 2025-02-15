048a0011e17161daf904321921e0ed38e5ef9b35
alialsaleh99 <ali.saleh@evolvextechnology.com>
2025-02-15 08:59:15 -0800

    add entity_id to branch

--- database/migrations/2025_02_13_074240_create_branches_table.php
+++ database/migrations/2025_02_13_074240_create_branches_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('branches', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('entity_id')->nullable()->constrained('entities');
             $table->string('address');
             $table->softDeletes();
             $table->timestamps();
